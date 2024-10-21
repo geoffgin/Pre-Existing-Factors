e87dc1b825542082209427986c09d225c77ea414
Natalie LaRowe <natalielarowe4@gmail.com>
2024-10-20 18:03:03 -0700

    All supervised work except deeper analysis on KNN regression

--- train_test.py
+++ train_test.py
@@ -35,7 +35,7 @@ def get_train_test():
     """
     X, y = prepare_X_y()
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state=0)
 
     return X_train, X_test, y_train, y_test
 
